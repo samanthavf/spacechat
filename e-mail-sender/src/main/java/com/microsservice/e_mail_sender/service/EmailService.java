f448e1fe62fb5476b502b65b7c294a185cfbc417
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-10-18 00:03:41 -0300

    emailservice commit

--- e-mail-sender/src/main/java/com/microsservice/e_mail_sender/service/EmailService.java
+++ e-mail-sender/src/main/java/com/microsservice/e_mail_sender/service/EmailService.java
@@ -1,5 +1,26 @@
 package com.microsservice.e_mail_sender.service;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class EmailService {
+	
+	private final JavaMailSender mailSender;
+	
+	public void sendValidationEmail(String toEmail, String token){
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(toEmail);
+		mailMessage.setSubject("Confirmação de Cadastro");
+		mailMessage.setText("Por favor, clique no link abaixo para validar sua conta:\n"
+                + "http://localhost:8080/validar?token=" + token);
+		
+		mailSender.send(mailMessage);
+	}
 
 }
