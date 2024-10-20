9ea24aed2f52b669b57ac643e7c0760b7e7f68c8
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-10-20 01:44:30 -0300

    first commit

---
+++ cadastro-microsservice/src/main/java/com/microsservice/cadastro/CadastroMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microsservice.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ cadastro-microsservice/src/main/java/com/microsservice/cadastro/models/UsersRequest.java
@@ -0,0 +1,25 @@
+package com.microsservice.cadastro.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "TABLE_USERS")
+public class UsersRequest {
+	@Id
+	@GeneratedValue(generator = "UUID")
+	private UUID id;
+	private String name;
+	private String email;
+	private String password;
+}
