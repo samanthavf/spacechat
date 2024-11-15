75e5849c6a7180367752db7f0fb2b9a7c326be8e
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-11-15 00:12:54 -0300

    sacanagem commit

--- e-mail-sender/src/main/java/com/microsservice/e_mail_sender/service/EmailService.java
+++ e-mail-sender/src/main/java/com/microsservice/e_mail_sender/service/EmailService.java
@@ -23,9 +23,9 @@ public class EmailService {
 	        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
 	        String link = "https://www.youtube.com/watch?v=7f76eIdqUdY&t=11s";
-	        //String link = "http://localhost:8081/verificado";
-	        String htmlMsg = "<h2>Confirmação de Cadastro</h2>"
-	                + "<p>Por favor, clique no botão abaixo para validar sua conta:</p>"
+	        //String link = "http://localhost:8081/verificado"; Confirmação de Cadastro , Por favor, clique no botão abaixo para validar sua conta:
+	        String htmlMsg = "<h2>Helloo its meee</h2>"
+	                + "<p>Olha o teste aê</p>"
 	                + "<a href='" + link + "' style='display:inline-block;padding:10px 20px;color:#fff;background-color:#4CAF50;"
 	                + "text-decoration:none;border-radius:5px;'>Validar Conta</a>";
 
