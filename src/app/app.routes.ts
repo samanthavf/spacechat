10576d2e911265ee6951c6561cb1a304a105da5e
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-11-23 00:46:32 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sign-up', component: SignUpComponent},
+    { path: 'sign-in', component: SignInComponent},
+    { path: '', redirectTo: '/sign-up', pathMatch: 'full' },
+    { path: '**', redirectTo: '/sign-in' } 
+];
