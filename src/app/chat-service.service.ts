426ec54a7d00c66643fff1012e1d6ec0e6a67440
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2025-01-31 14:59:01 -0300

    last commit

--- src/app/chat-service.service.ts
+++ src/app/chat-service.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import { BehaviorSubject } from 'rxjs';
+import SockJS from 'sockjs-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
+  private stompClient!: Client;
+  private messageSubject = new BehaviorSubject<string>('');
+  public messages$ = this.messageSubject.asObservable();
+  constructor() {
+    this.connect();
+   }
 
-  constructor() { }
+  private connect(){
+    this.stompClient = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8084/buildrun-livechat'),
+      reconnectDelay: 500
+    });
+
+    this.stompClient.onConnect = () =>{
+      console.log('Conectado ao WebSocket!');
+
+      // Assina o tópico para receber mensagens
+      this.stompClient.subscribe('/topics/livechat', (message) => {
+        this.messageSubject.next(message.body);
+      });
+    };
+
+    this.stompClient.activate();
+  }
+
+// Enviar mensagem para o servido
+sendMessage(message: string){
+  if(this.stompClient.connected){
+    this.stompClient.publish({destination: '/app/new-message', body:message});
+  }else{
+    console.warn('WebSocket não está conectado.')
+  }
+}
+
 }
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ChatServiceService } from '../chat-service.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
+message: string = '';
+messages: string[] = []
+
+constructor(private service: ChatServiceService){}
+
+ngOnInt(){
+  this.service.messages$.subscribe((message) => {
+    if(message){
+      this.messages.push(message);
+    }
+  });
+}
+
+sendMessage(){
+  if(this.message.trim()){
+    this.service.sendMessage(this.message);
+    this.message ='';
+  }
+}
 
 }
