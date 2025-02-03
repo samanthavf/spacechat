3087c5ea76a933c20dd896a7ecd2caa3dd9ca1c9
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2025-02-03 15:44:21 -0300

    layout commit

--- src/app/chat-component/chat-component.component.ts
+++ src/app/chat-component/chat-component.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-component',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './chat-component.component.html',
   styleUrl: './chat-component.component.css'
 })
 export class ChatComponentComponent {
+message: any;
+
+  constructor(private renderer: Renderer2,@Inject(PLATFORM_ID) private platformId: Object){}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.addClass(document.body, 'chat');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.removeClass(document.body, 'chat');
+    }
+  }
+
+  sendMessage() {
+    throw new Error('Method not implemented.');
+    }
 
 }
